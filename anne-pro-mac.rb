c16f129f130de6cd92bd39c99c45cbd90e346312
boggs <hello@boggs.xyz>
2018-03-07 19:47:17 +0800

    Add Casks for AnnePro-mac and Ivacy

---
+++ anne-pro-mac.rb
@@ -0,0 +1,10 @@
+cask 'anne-pro-mac' do
+  version '0.2'
+  sha256 '2065242f8cfbee7760eaf28e9fb155621cb25302f9b3190bc5bb3e68abbaccc7'
+
+  url 'https://github.com/msvisser/AnnePro-mac/releases/download/v0.2/AnnePro.app.zip'
+  name 'AnnePro-mac'
+  homepage 'https://github.com/msvisser/AnnePro-mac'
+
+  app 'AnnePro.app'
+end
---
+++ ivacy.rb
@@ -0,0 +1,10 @@
+cask 'ivacy' do
+  version '5.0.1'
+  sha256 '068ebf324bd52ca0a28d222af17672b475e575093341e1ff491a9b2aa00b4a22'
+
+  url 'https://s3.amazonaws.com/apps-ivacy/mac/Ivacy.dmg'
+  name 'Ivacy'
+  homepage 'https://www.ivacy.com/vpn-for-mac/'
+
+  app 'Ivacy.app'
+end
