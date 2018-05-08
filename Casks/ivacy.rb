b54b9f1a71bd3e27e3708906ba48096fa13f7bfe
boggs <hello@boggs.xyz>
2018-05-08 15:45:00 +0800

    [ivacy] Change ivacy to :latest and :no_check

--- Casks/ivacy.rb
+++ Casks/ivacy.rb
@@ -1,6 +1,6 @@
 cask 'ivacy' do
-  version '5.0.1'
-  sha256 '068ebf324bd52ca0a28d222af17672b475e575093341e1ff491a9b2aa00b4a22'
+  version :latest
+  sha256 :no_check
 
   url 'https://s3.amazonaws.com/apps-ivacy/mac/Ivacy.dmg'
   name 'Ivacy'
